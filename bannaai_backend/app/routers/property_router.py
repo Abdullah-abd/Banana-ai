2ada1672796bd448754fe4359171ec6aa27b04e9
Abdullah <abdullah262001@gmail.com>
2025-10-19 08:59:06 +0530

    route to read properties

--- bannaai_backend/app/routers/property_router.py
+++ bannaai_backend/app/routers/property_router.py
@@ -9,3 +9,6 @@ router = APIRouter(prefix="/properties", tags=["properties"])
 @router.post("/", response_model=PropertyRead)
 def add_property(property: PropertyCreate, db: Session = Depends(get_db)):
     return create_property(db, property)
+@router.get("/", response_model=list[PropertyRead])
+def read_properties(db: Session = Depends(get_db)):
+    return get_properties_sorted(db)
