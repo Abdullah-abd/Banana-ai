04db7994220ab069a1dba7b00632ebce22f3a0da
Abdullah <abdullah262001@gmail.com>
2025-10-23 09:05:13 +0530

    bug fixed

--- bannaai_backend/app/models/property.py
+++ bannaai_backend/app/models/property.py
@@ -9,6 +9,8 @@ class Property(Base):
     area_sqft = Column(Float, nullable=False)
     price = Column(Float, nullable=False)
 
-    @property
-    def price_per_sqft(self):
-        return self.price / self.area_sqft
+@property
+def price_per_sqft(self):
+    if not self.area_sqft or self.area_sqft == 0:
+        return 0
+    return self.price / self.area_sqft
